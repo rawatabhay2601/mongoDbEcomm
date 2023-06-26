792ad946cce1cf6257424587c8b8ae850841a520
rawatabhay2601 <rawatabhay2601@gmail.com>
2023-06-26 18:05:16 +0530

    Debugging

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ mongoose
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
+          name: 'Abhay',
+          email: 'abhay@test.com',
           cart: {
             items: []
           }
